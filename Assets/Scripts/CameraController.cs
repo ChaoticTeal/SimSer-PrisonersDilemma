c366f8dd7b1785b23829e14356c2a02464ca0f2f
PurpleNinjaLink <jawhittom@gmail.com>
2018-02-02 14:21:31 -0600

    Basic Functionality Update
    Player movement is functional for both players. The camera currently soars into the stratosphere as soon as either of them moves, something isn't calculating right in the movement script.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+
+    // SerializeFields for assignment in-editor
+    // Camera movement speed
+    [SerializeField] float cameraSpeed = 5f;
+    // Maximum vertical position of camera
+    [SerializeField] float maxY = 10f;
+    // Minimum vertical position of camera
+    [SerializeField] float minY = 0f;
+    // List of things that must be on camera
+    [SerializeField] List<Transform> targets;
+
+    // Private fields
+    // Average vertical position of targets
+    float midY;
+    // Total vertical position of all targets
+    float totalY;
+    // Number of targets
+    int totalTargets;
+    // Position to move to
+    Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+        // Get the total number of elements in the list
+        totalTargets = targets.Count;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Move();
+	}
+
+    private void Move()
+    {
+        // Add up all target vertical positions
+        foreach(Transform t in targets)
+            totalY += t.position.y;
+        // Divide by number of targets to find average
+        midY = totalY / totalTargets;
+        // Set up new position vector
+        newPosition = new Vector3(transform.position.x, midY, transform.position.z);
+        // Move to average vertical position
+        transform.position = Vector3.Lerp(transform.position, newPosition, cameraSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+    // SerializeFields for assignment in-editor
+    // Player movement speed
+    [SerializeField] float speed = 5f;
+    // Player identification string
+    [Tooltip("Must be either P1 or P2")]
+    [SerializeField] string playerID = "P1";
+
+    // Private fields
+    // Horizontal input
+    float horizontalInput;
+    // Vertical input
+    float verticalInput;
+    // Player Rigidbody
+    Rigidbody2D rigidbody;
+    // Name of cancel button
+    string cancelButton;
+    // Name of confirm button
+    string confirmButton;
+    // Name of horizontal movement axis
+    string horizontalAxis;
+    // Name of vertical movement axis
+    string verticalAxis;
+
+	// Use this for initialization
+	void Start () 
+	{
+        // Set axis and button names - this allows the code to work for either player
+        horizontalAxis = string.Format("{0}-Horizontal", playerID);
+        verticalAxis = string.Format("{0}-Vertical", playerID);
+        confirmButton = string.Format("{0}-Confirm", playerID);
+        cancelButton = string.Format("{0}-Cancel", playerID);
+
+        // Get reference to rigidbody
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Move();
+	}
+
+    // Checks input and moves player
+    void Move()
+    {
+        horizontalInput = Input.GetAxis(horizontalAxis);
+        verticalInput = Input.GetAxis(verticalAxis);
+        rigidbody.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime);
+    }
+}
