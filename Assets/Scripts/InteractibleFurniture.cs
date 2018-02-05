f4cfb05db3faa38edfefcedf739f59f2d6fdb299
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2018-02-05 12:42:26 -0600

    Potential Fixes
    This reverts the revert.

---
+++ Assets/Scripts/InteractibleFurniture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleFurniture : MonoBehaviour 
+{
+    // Private fields
+    // Does the furniture have the key?
+    bool hasKey;
+    // Does the furniture have the safe?
+    bool hasSafe;
+
+    // Properties for access in other classes
+    // Does the furniture have the key?
+    public bool HasKey
+    {
+        get
+        {
+            return hasKey;
+        }
+        set
+        {
+            hasKey = value;
+        }
+    }
+
+    // Does the furniture have the safe?
+    public bool HasSafe
+    {
+        get
+        {
+            return hasSafe;
+        }
+        set
+        {
+            hasSafe = value;
+        }
+    }
+}
