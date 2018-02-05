f91b3cecfd4ddddb2f9bffdc33a94fef4cbdf751
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2018-02-05 11:52:16 -0600

    UI Additions
    Added in where instructions on how to move, pop ups for when players are near to interactable objects, and a base for the main informational area.

---
+++ Assets/Scripts/PopUps.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUps : MonoBehaviour {
+
+    [SerializeField]
+    Canvas popUpCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+        popUpCanvas.enabled = false;
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            popUpCanvas.enabled = true;
+        }
+
+        if(Input.GetButton("P1-Confirm") || Input.GetButton("P2-Confirm"))
+        {
+            popUpCanvas.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            popUpCanvas.enabled = false;
+        }
+    }
+
+}
